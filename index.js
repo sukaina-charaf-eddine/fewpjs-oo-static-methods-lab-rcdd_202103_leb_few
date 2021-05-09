e234d26d8163111627c9f56ee18a220d235f60ce
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-05-09 12:44:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.slice( 1 )
 
   }
-  static sanitize(string){
-    return string.remove( /[^A-Za-z0-9 '-]/g, '' )
-
-  }
+  static sanitize( string ) {
+     return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+   }
   static titleize( sentence ) {
     let except = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let result = [];
